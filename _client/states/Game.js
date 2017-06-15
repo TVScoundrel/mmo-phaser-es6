da21162c8d62e87a19e40ee62e2c8218ba2f8d49
githubschman <sarahhubschmail@gmail.com>
2017-06-14 22:18:34 -0400

    goodbye toooo youuuu

--- _client/states/Game.js
+++ _client/states/Game.js
@@ -1,26 +1,46 @@
 import Client from '../services/Client'
 
+var tealTeam;
+var orangeTeam;
+
 class Game extends Phaser.State {
     constructor() {
         super()
         this.client = new Client(this)
         this.playerMap = {}
+
+        this.state = {
+            team: ''
+        }
     }
+   
+
 
     preload() {
         this.game.stage.disableVisibilityChange = true
         this.game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON)
         this.game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32)
-        this.game.load.image('sprite','assets/sprites/orange-player.png')
+        this.game.load.image('orangeSprite','assets/sprites/orange-player.png')
+        this.game.load.image('tealSprite','assets/sprites/teal-player.png')
     }
 
     create() {
+
+        tealTeam = this.game.add.group();
+        orangeTeam = this.game.add.group();
+
+        let coin = Math.floor(Math.random()*2)
+        if(coin === 0){this.state.team = 'orange'}
+        else{this.state.team = 'teal'}
+
+
         var map = this.game.add.tilemap('map')
         map.addTilesetImage('tilesheet', 'tileset')
         var layer
         for(var i = 0; i < map.layers.length; i++) {
             layer = map.createLayer(i)
         }
+
         layer.inputEnabled = true
         this.client.askNewPlayer()
 
@@ -32,7 +52,12 @@ class Game extends Phaser.State {
     }
 
     addNewPlayer(id, x, y) {
-        this.playerMap[id] = this.game.add.sprite(x, y, 'sprite');
+        if(this.state.team === 'orange'){this.playerMap[id] = this.game.add.sprite(x, y, 'orangeSprite')}
+                                    else{this.playerMap[id] = this.game.add.sprite(x, y, 'tealSprite')}
+        
+        if(this.state.team === 'orange'){this.orangeTeam.add(this.playerMap[id])}
+                                    else{this.tealTeam.add(this.playerMap[id])}
+
         this.playerMap[id].width = 25;
         this.playerMap[id].height = 25;
     }
@@ -40,7 +65,7 @@ class Game extends Phaser.State {
     movePlayer = function(id, x, y){
         var player = this.playerMap[id];
         var distance = Phaser.Math.distance(player.x, player.y, x, y);
-        var duration = distance * 10;
+        var duration = distance * 5; //correlate to this.playerMap[id].width
         var tween = this.game.add.tween(player);
         tween.to({ x, y }, duration);
         tween.start();
