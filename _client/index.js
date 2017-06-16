015f933b9e69db31bc20aa6221265fe1d75667b9
JHFellows <j.fellows88@gmail.com>
2017-06-16 18:47:22 -0400

    finally decided on this size yesss

--- _client/index.js
+++ _client/index.js
@@ -3,8 +3,9 @@ import Game from './states/Game'
 
 class App extends Phaser.Game {
     constructor() {
-        //super(24*32, 17*32, Phaser.AUTO); Phaser.WEBGL_MULTI
-        super(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO);
+        super(1950, 1070, Phaser.AUTO);
+        //super(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO);
+
         this.state.add('Fonts', Fonts)
         this.state.add('Game', Game)
         this.state.start('Fonts')
